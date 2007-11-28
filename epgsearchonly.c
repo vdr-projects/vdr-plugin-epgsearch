39b83ace1bfe87f260843c161c0da2844d6efe14
Christian Wieninger <winni@debian.(none)>
2007-11-28 19:11:38 +0100

    - formatting with indent

--- epgsearchonly.c
+++ epgsearchonly.c
@@ -31,115 +31,129 @@ The project's page is at http://winni.vdr-developer.org/epgsearch
 #include "services.h"
 #include "mainmenushortcut.h"
 
-static const char VERSION[]        = "0.0.1";
-static const char DESCRIPTION[]    = trNOOP("Direct access to epgsearch's search menu");
-static const char MAINMENUENTRY[]  = trNOOP("Search");
-static const char SETUPTEXT[]      = trNOOP("EpgSearch-Search in main menu");
+static const char *VERSION       = "0.0.1";
+static const char *DESCRIPTION   = trNOOP("Direct access to epgsearch's search menu");
+static const char *MAINMENUENTRY = trNOOP("Search");
+static const char *SETUPTEXT     = trNOOP("EpgSearch-Search in main menu");
 
 #if APIVERSNUM < 10507
-const tI18nPhrase PhrasesLocal[] =
-{
-    {
-        "Direct access to epgsearch's search menu",
-        "Direkter Zugriff auf epgsearch's Suchenmenu",// Deutsch
-        "",// TODO 
-        "",// TODO
-        "",// TODO
-        "",// TODO 
-        "",// TODO
-        "",// TODO 
-        "Suoratoiminto EPGSearch-laajennoksen haulle",
-        "",// TODO Polski
-        "",// TODO Español
-        "",// TODO ÅëëçíéêÜ (Greek)
-        "",// TODO Svenska
-        "",// TODO Romaneste
-        "",// TODO Magyar
-        "",// TODO Català
-        "",// TODO ÀãááÚØÙ (Russian)
-        "",// TODO Hrvatski (Croatian)
-        "", // Eesti
-        "", // Dansk
-        "", // Czech
-    },
-    {
-        "Search",
-        "Suche",
-        "",// TODO
-        "Ricerca",
-        "Zoek",
-        "",// TODO
-        "Recherche",
-        "",// TODO
-        "Etsi ohjelmaoppaasta",
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "", // Eesti
-        "", // Dansk
-        "", // Czech
-    },
-    {
-        "EpgSearch-Search in main menu",
-        "EpgSearch-Suche im Hauptmenü",// Deutsch
-        "",// TODO 
-        "",// TODO
-        "",// TODO
-        "",// TODO 
-        "",// TODO
-        "",// TODO 
-        "EpgSearch-laajennoksen hakutoiminto päävalikossa",
-        "",// TODO Polski
-        "",// TODO Español
-        "",// TODO ÅëëçíéêÜ (Greek)
-        "",// TODO Svenska
-        "",// TODO Romaneste
-        "",// TODO Magyar
-        "",// TODO Català
-        "",// TODO ÀãááÚØÙ (Russian)
-        "",// TODO Hrvatski (Croatian)
-        "", // Eesti
-        "", // Dansk
-        "", // Czech
-    },
-    { NULL }
+const tI18nPhrase PhrasesLocal[] = {
+  {
+   "Direct access to epgsearch's search menu",
+   "Direkter Zugriff auf epgsearch's Suchenmenu",	// Deutsch
+   "",				// TODO 
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO 
+   "",				// TODO
+   "",				// TODO 
+   "Suoratoiminto EPGSearch-laajennoksen haulle",
+   "",				// TODO Polski
+   "",				// TODO Español
+   "",				// TODO ÅëëçíéêÜ (Greek)
+   "",				// TODO Svenska
+   "",				// TODO Romaneste
+   "",				// TODO Magyar
+   "",				// TODO Català
+   "",				// TODO ÀãááÚØÙ (Russian)
+   "",				// TODO Hrvatski (Croatian)
+   "",				// Eesti
+   "",				// Dansk
+   "",				// Czech
+   },
+  {
+   "Search",
+   "Suche",
+   "",				// TODO
+   "Ricerca",
+   "Zoek",
+   "",				// TODO
+   "Recherche",
+   "",				// TODO
+   "Etsi ohjelmaoppaasta",
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// Eesti
+   "",				// Dansk
+   "",				// Czech
+   },
+  {
+   "EpgSearch-Search in main menu",
+   "EpgSearch-Suche im Hauptmenü",	// Deutsch
+   "",				// TODO 
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO 
+   "",				// TODO
+   "",				// TODO 
+   "EpgSearch-laajennoksen hakutoiminto päävalikossa",
+   "",				// TODO Polski
+   "",				// TODO Español
+   "",				// TODO ÅëëçíéêÜ (Greek)
+   "",				// TODO Svenska
+   "",				// TODO Romaneste
+   "",				// TODO Magyar
+   "",				// TODO Català
+   "",				// TODO ÀãááÚØÙ (Russian)
+   "",				// TODO Hrvatski (Croatian)
+   "",				// Eesti
+   "",				// Dansk
+   "",				// Czech
+   },
+  {NULL}
 };
 #endif
-  
-class cPluginEpgsearchonly: public cMainMenuShortcut
-{
-    public:
-        virtual const char* Version() { return VERSION; }
-#if APIVERSNUM < 10507
-        virtual const char* Description() { return tr(DESCRIPTION); }
-#else
-        virtual const char* Description() { return I18nTranslate(DESCRIPTION, I18nEpgsearch); }
-#endif
-        virtual bool Initialize();
-        virtual cOsdObject *MainMenuAction() { return GetEpgSearchMenu("Epgsearch-searchmenu-v1.0"); };
 
-    protected:
+class cPluginEpgsearchonly:public cMainMenuShortcut {
+public:
+  virtual const char *Version() {
+    return VERSION;
+  }
 #if APIVERSNUM < 10507
-        virtual const char* SetupText() { return tr(SETUPTEXT); }
-        virtual const char* MainMenuText() { return tr(MAINMENUENTRY); };
+  virtual const char *Description() {
+    return tr(DESCRIPTION);
+  }
 #else
-        virtual const char* SetupText() { return I18nTranslate(SETUPTEXT, I18nEpgsearch); }
-        virtual const char* MainMenuText() { return I18nTranslate(MAINMENUENTRY, I18nEpgsearch); }
+  virtual const char *Description() {
+    return I18nTranslate(DESCRIPTION, I18nEpgsearch);
+  }
+#endif
+  virtual bool Initialize();
+  virtual cOsdObject *MainMenuAction() {
+    return GetEpgSearchMenu("Epgsearch-searchmenu-v1.0");
+  };
+
+protected:
+#if APIVERSNUM < 10507
+  virtual const char *SetupText() {
+    return tr(SETUPTEXT);
+  }
+  virtual const char *MainMenuText() {
+    return tr(MAINMENUENTRY);
+  };
+#else
+  virtual const char *SetupText() {
+    return I18nTranslate(SETUPTEXT, I18nEpgsearch);
+  }
+  virtual const char *MainMenuText() {
+    return I18nTranslate(MAINMENUENTRY, I18nEpgsearch);
+  }
 #endif
 };
 
 bool cPluginEpgsearchonly::Initialize()
 {
 #if APIVERSNUM < 10507
-    RegisterI18n(PhrasesLocal);
+  RegisterI18n(PhrasesLocal);
 #endif
-    return cMainMenuShortcut::Initialize();
+  return cMainMenuShortcut::Initialize();
 }
 
-VDRPLUGINCREATOR(cPluginEpgsearchonly); // Don't touch this!
+VDRPLUGINCREATOR(cPluginEpgsearchonly);	// Don't touch this!
