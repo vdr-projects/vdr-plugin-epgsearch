9bfc5c2f98b8d1529467c0eb64690b7947b1fd3a
Christian Wieninger <winni@debian.(none)>
2007-11-28 19:09:49 +0100

    - formatting with 'indent -n2 -l100 -kr'

--- conflictcheckonly.c
+++ conflictcheckonly.c
@@ -31,197 +31,202 @@ The project's page is at http://winni.vdr-developer.org/epgsearch
 #include "services.h"
 #include "mainmenushortcut.h"
 
-static const char VERSION[]        = "0.0.1";
-static const char DESCRIPTION[]    = trNOOP("Direct access to epgsearch's conflict check menu");
-static const char MAINMENUENTRY[]  = trNOOP("Timer conflicts");
-static const char SETUPTEXT[]      = trNOOP("Conflict info in main menu");
+static const char VERSION[]       = "0.0.1";
+static const char DESCRIPTION[]   = trNOOP("Direct access to epgsearch's conflict check menu");
+static const char MAINMENUENTRY[] = trNOOP("Timer conflicts");
+static const char SETUPTEXT[]     = trNOOP("Conflict info in main menu");
 
 #if APIVERSNUM < 10507
-const tI18nPhrase PhrasesLocal[] =
-{
-    {
-        "Direct access to epgsearch's conflict check menu",
-        "Direkter Zugriff auf epgsearch's Konflikt-Prüfungs-Menü",// Deutsch
-        "",// TODO 
-        "",// TODO
-        "",// TODO
-        "",// TODO 
-        "",// TODO
-        "",// TODO 
-        "Suoratoiminto EPGSearch-laajennoksen ajastimien tarkistukselle",
-        "",// TODO Polski
-        "",// TODO Español
-        "",// TODO ÅëëçíéêÜ (Greek)
-        "",// TODO Svenska
-        "",// TODO Romaneste
-        "",// TODO Magyar
-        "",// TODO Català
-        "",// TODO ÀãááÚØÙ (Russian)
-        "",// TODO Hrvatski (Croatian)
-        "", // Eesti
-        "", // Dansk
-        "", // Czech
-    },
-    {
-        "Timer conflicts",
-        "Timer-Konflikte",
-        "",// TODO
-        "",// TODO
-        "",
-        "",// TODO
-        "",
-        "",// TODO
-        "Ajastimien päällekkäisyydet",
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "", // Eesti
-        "", // Dansk
-        "", // Czech
-    },
-    {
-        "next",
-        "nächster",
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "seuraava",
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "", // Eesti
-        "", // Dansk
-        "", // Czech
-    },
-    {
-        "Conflict info in main menu",
-        "Konflikt-Info im Hauptmenü",
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "Näytä päällekkäisyydet päävalikossa",
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "",// TODO
-        "", // Eesti
-        "", // Dansk
-        "", // Czech
-    },
-    { NULL }
+const tI18nPhrase PhrasesLocal[] = {
+  {
+   "Direct access to epgsearch's conflict check menu",
+   "Direkter Zugriff auf epgsearch's Konflikt-Prüfungs-Menü",	// Deutsch
+   "",				// TODO 
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO 
+   "",				// TODO
+   "",				// TODO 
+   "Suoratoiminto EPGSearch-laajennoksen ajastimien tarkistukselle",
+   "",				// TODO Polski
+   "",				// TODO Español
+   "",				// TODO ÅëëçíéêÜ (Greek)
+   "",				// TODO Svenska
+   "",				// TODO Romaneste
+   "",				// TODO Magyar
+   "",				// TODO Català
+   "",				// TODO ÀãááÚØÙ (Russian)
+   "",				// TODO Hrvatski (Croatian)
+   "",				// Eesti
+   "",				// Dansk
+   "",				// Czech
+   },
+  {
+   "Timer conflicts",
+   "Timer-Konflikte",
+   "",				// TODO
+   "",				// TODO
+   "",
+   "",				// TODO
+   "",
+   "",				// TODO
+   "Ajastimien päällekkäisyydet",
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// Eesti
+   "",				// Dansk
+   "",				// Czech
+   },
+  {
+   "next",
+   "nächster",
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "seuraava",
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// Eesti
+   "",				// Dansk
+   "",				// Czech
+   },
+  {
+   "Conflict info in main menu",
+   "Konflikt-Info im Hauptmenü",
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "Näytä päällekkäisyydet päävalikossa",
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// TODO
+   "",				// Eesti
+   "",				// Dansk
+   "",				// Czech
+   },
+  {NULL}
 };
 #endif
-  
+
 cString DateTime(time_t t)
 {
-    char buffer[32];
-    if (t == 0)
-    {
-        time(&t);
-    }
-    struct tm tm_r;
-    tm *tm = localtime_r(&t, &tm_r);
-    snprintf(buffer, sizeof(buffer), "%02d.%02d %02d:%02d", tm->tm_mday, tm->tm_mon + 1, tm->tm_hour, tm->tm_min);
-    return buffer;
+  char buffer[32];
+  if (t == 0) {
+    time(&t);
+  }
+  struct tm tm_r;
+  tm *tm = localtime_r(&t, &tm_r);
+  snprintf(buffer, sizeof(buffer), "%02d.%02d %02d:%02d", tm->tm_mday,
+	   tm->tm_mon + 1, tm->tm_hour, tm->tm_min);
+  return buffer;
 }
 
-class cPluginConflictcheckonly : public cMainMenuShortcut
-{
-    private:
-        char* _menuText;
+class cPluginConflictcheckonly:public cMainMenuShortcut {
+private:
+  char *_menuText;
 
-    public:
-        cPluginConflictcheckonly();
-        ~cPluginConflictcheckonly();
-        virtual const char* Version() { return VERSION; }
-
+public:
+   cPluginConflictcheckonly();
+  ~cPluginConflictcheckonly();
+  virtual const char *Version() {
+    return VERSION;
+  }
 #if APIVERSNUM < 10507
-        virtual const char* Description() { return tr(DESCRIPTION); }
+  virtual const char *Description() {
+    return tr(DESCRIPTION);
+  }
 #else
-        virtual const char* Description() { return I18nTranslate(DESCRIPTION, I18nEpgsearch); }
+  virtual const char *Description() {
+    return I18nTranslate(DESCRIPTION, I18nEpgsearch);
+  }
 #endif
-        virtual bool Initialize();
-        virtual cOsdObject* MainMenuAction() { return GetEpgSearchMenu("Epgsearch-conflictmenu-v1.0"); }
+  virtual bool Initialize();
+  virtual cOsdObject *MainMenuAction() {
+    return GetEpgSearchMenu("Epgsearch-conflictmenu-v1.0");
+  }
 
-    protected:
+protected:
 #if APIVERSNUM < 10507
-        virtual const char* SetupText() { return tr(SETUPTEXT); }
+  virtual const char *SetupText() {
+    return tr(SETUPTEXT);
+  }
 #else
-        virtual const char* SetupText() { return I18nTranslate(SETUPTEXT, I18nEpgsearch); }
+  virtual const char *SetupText() {
+    return I18nTranslate(SETUPTEXT, I18nEpgsearch);
+  }
 #endif
-        virtual const char* MainMenuText(void);
+  virtual const char *MainMenuText(void);
 };
 
-cPluginConflictcheckonly::cPluginConflictcheckonly()
-:_menuText(NULL)
+cPluginConflictcheckonly::cPluginConflictcheckonly():_menuText(NULL)
 {
 }
 
 cPluginConflictcheckonly::~cPluginConflictcheckonly()
 {
-    free(_menuText);
+  free(_menuText);
 }
 
-const char* cPluginConflictcheckonly::MainMenuText(void)
+const char *cPluginConflictcheckonly::MainMenuText(void)
 {
 #if APIVERSNUM < 10507
-    const char* menuText = tr(MAINMENUENTRY);
+  const char *menuText = tr(MAINMENUENTRY);
 #else
-    const char* menuText = I18nTranslate(MAINMENUENTRY, I18nEpgsearch);
+  const char *menuText = I18nTranslate(MAINMENUENTRY, I18nEpgsearch);
 #endif
 
-    cPlugin *epgSearchPlugin = cPluginManager::GetPlugin("epgsearch");
-    if (epgSearchPlugin)
-    {
-        Epgsearch_lastconflictinfo_v1_0* serviceData = new Epgsearch_lastconflictinfo_v1_0;
-        if (epgSearchPlugin->Service("Epgsearch-lastconflictinfo-v1.0", serviceData))
-        {
-            if (serviceData->relevantConflicts > 0)
-            {
-                free(_menuText);
-                asprintf(&_menuText, "%s (%d, %s: %s)", menuText, serviceData->relevantConflicts, 
+  cPlugin *epgSearchPlugin = cPluginManager::GetPlugin("epgsearch");
+  if (epgSearchPlugin) {
+    Epgsearch_lastconflictinfo_v1_0 *serviceData = new Epgsearch_lastconflictinfo_v1_0;
+    if (epgSearchPlugin->Service("Epgsearch-lastconflictinfo-v1.0", serviceData)) {
+      if (serviceData->relevantConflicts > 0) {
+	free(_menuText);
+	asprintf(&_menuText, "%s (%d, %s: %s)", menuText, serviceData->relevantConflicts,
 #if APIVERSNUM < 10507
-                  tr("next"), *DateTime(serviceData->nextConflict));
+		 tr("next"), *DateTime(serviceData->nextConflict));
 #else
-                  I18nTranslate("next", I18nEpgsearch), *DateTime(serviceData->nextConflict));
+		 I18nTranslate("next", I18nEpgsearch), *DateTime(serviceData->nextConflict));
 #endif
-                menuText = _menuText;
-            }
-        }
-        delete serviceData;
+	menuText = _menuText;
+      }
     }
-    return menuText;
+    delete serviceData;
+  }
+  return menuText;
 }
 
 bool cPluginConflictcheckonly::Initialize(void)
 {
 #if APIVERSNUM < 10507
-    RegisterI18n(PhrasesLocal);
+  RegisterI18n(PhrasesLocal);
 #endif
-    return cMainMenuShortcut::Initialize();
+  return cMainMenuShortcut::Initialize();
 }
 
-VDRPLUGINCREATOR(cPluginConflictcheckonly); // Don't touch this!
+VDRPLUGINCREATOR(cPluginConflictcheckonly);	// Don't touch this!
