1e15e02e602a96d71c0a034acab7273416810e5d
Christian Wieninger <cwieninger (at) gmx (dot) de>
2008-02-11 18:42:37 +0100

    0.9.24.beta18

--- epgsearch.c
+++ epgsearch.c
@@ -64,7 +64,7 @@ The project's page is at http://winni.vdr-developer.org/epgsearch
 #include "menu_quicksearch.h"
 #include "menu_announcelist.h"
 
-static const char VERSION[]        = "0.9.24.beta17";
+static const char VERSION[]        = "0.9.24.beta18";
 static const char DESCRIPTION[]    =  trNOOP("search the EPG for repeats and more");
 
 // globals
