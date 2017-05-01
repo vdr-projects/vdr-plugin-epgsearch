72ca5b0e6f70632b834ec8ecf85bcfea7d38f900
Frank Neumann <fnu@yavdr.org>
2017-05-01 20:56:56 +0200

    push version to 1.0.2, even number for stable vdr-2.2.x branch.

--- epgsearch.c
+++ epgsearch.c
@@ -69,7 +69,7 @@ The project's page is at http://winni.vdr-developer.org/epgsearch
 #include "confdloader.h"
 #include "pending_notifications.h"
 
-static const char VERSION[]        = "1.0.1.beta5";
+static const char VERSION[]        = "1.0.2";
 static const char DESCRIPTION[]    =  trNOOP("search the EPG for repeats and more");
 
 // globals
